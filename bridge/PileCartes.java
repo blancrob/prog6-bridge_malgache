21f809ad0363908950ac0ed906a2cda4f9f884f8
Benjamin <benjamincoste@outlook.fr>
2018-05-05 18:53:30 +0200

    Correction aleatoire(couleur) dans pileCartes

--- bridge/PileCartes.java
+++ bridge/PileCartes.java
@@ -144,8 +144,6 @@ public class PileCartes {
         Carte tmp;
         Iterator<Carte> it = pile.iterator();
         
-        res = it.next();
-        
         do{
             tmp = it.next();
             if(tmp.couleur == couleur){
