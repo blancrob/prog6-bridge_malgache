075b72ad2410904aebcfd6647effdc978a15ba7d
Benjamin <benjamincoste@outlook.fr>
2018-05-19 13:49:12 +0200

    Creation de EtatGlobal

---
+++ bridge/EtatGlobal.java
@@ -0,0 +1,14 @@
+package bridge;
+
+import java.io.Serializable;
+
+public class EtatGlobal implements Serializable{
+    Configuration config;
+    Joueur j1, j2;
+    
+    public EtatGlobal(Configuration config, Joueur j1, Joueur j2){
+        this.config = config;
+        this.j1 = j1;
+        this.j2 = j2;
+    }
+}
