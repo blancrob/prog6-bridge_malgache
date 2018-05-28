d6872354f4b7b55565a7eb1fb7707544128a2cbd
Lythz <florent.haudry@gmail.com>
2018-05-28 13:24:45 +0200

    animation pioche IA

--- bridge/MenuJeu.java
+++ bridge/MenuJeu.java
@@ -32,6 +32,7 @@ public class MenuJeu extends Parent {
     
     Button quit;
     Button save;
+    Button load;
     Button option;
     Button restart;
     Button rules;
@@ -227,6 +228,7 @@ public class MenuJeu extends Parent {
         quit.setTranslateX(largeur_scene/48);
         quit.setTranslateY(hauteur_scene-hauteur_scene/10);
         this.getChildren().add(quit);
+        //quit.setStyle("-fx-color: WHITE; -fx-decoration: none;");
         quit.setOnKeyPressed(keyEvent ->{
             KeyCode q = keyEvent.getCode();
             if (q.equals(KeyCode.Q)){
@@ -238,11 +240,18 @@ public class MenuJeu extends Parent {
         });
         
         save = new Button("Sauvegarder");
-        save.setPrefWidth(largeur_scene/6.4);
+        save.setPrefWidth(largeur_scene/13.715);
         save.setPrefHeight(hauteur_scene/12);
-        save.setTranslateX(largeur_scene/48);
+        save.setTranslateX(largeur_scene/9.6);
         save.setTranslateY(hauteur_scene-hauteur_scene/5);
         this.getChildren().add(save);
+        
+        load = new Button("Charger");
+        load.setPrefWidth(largeur_scene/13.715);
+        load.setPrefHeight(hauteur_scene/12);
+        load.setTranslateX(largeur_scene/48);
+        load.setTranslateY(hauteur_scene-hauteur_scene/5);
+        this.getChildren().add(load);
 
         option = new Button("Options");
         option.setPrefWidth(largeur_scene/13.715);
